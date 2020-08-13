005474abf6744b745a21bc337297820dbf888a4b
ilho, Jeong <ihjeong.dev@gmail.com>
2020-08-14 02:01:47 +0900

    add todoInput Modal

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -5,14 +5,26 @@
         <span class="addContainer" v-on:click="addTodo">
             <i class="addBtn fas fa-plus" aria-hidden="true"></i>
         </span>
+
+        <modal v-if="showModal" @close="showModal = false">
+            <h3 slot="header">경고</h3>
+            <span slot="body"></span>
+            <span slot="footer" @click="showModal = false">
+                할 일을 입력하세요.
+                <i class="closeModalBtn fas fa-times" aria-hidden="true"></i>
+            </span>
+        </modal>
     </div>
 </template>
 
 <script>
+import Modal from './common/Modal.vue'
+
 export default {
     data() {
         return {
-            newTodoItem: ''
+            newTodoItem: '',
+            showModal: false
         }
     },
     methods: {
@@ -22,11 +34,16 @@ export default {
                 // localStorage.setItem(value, value);
                 this.$emit('addTodo', value);
                 this.clearInput();
+            } else {
+                this.showModal = !this.showModal;
             }
         },
         clearInput() {
             this.newTodoItem = '';
         }
+    },
+    components: {
+        modal: Modal
     }
 }
 </script>
@@ -55,5 +72,10 @@ export default {
     .addBtn {
         color: white;
         vertical-align: middle;
+    }
+    .closeModalBtn {
+        line-height: 45px;
+        color: #42b983;
+        margin-right: 5px;
     }
 </style>
